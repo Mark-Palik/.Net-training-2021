0fb399a76cb86082ec28d2a59ee6349c375a3183
Mark-Palik <Mark.Palik9322@gmail.com>
2021-03-10 13:58:58 +0300

    3rd lab

---
+++ 3rd lab/3rd lab/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3rd_lab
+{
+    class Circle
+    {
+        public double Radius { get;}
+        public Circle(double r)
+        {
+            if (r <= 0)
+            {
+                throw new ArgumentException("Radius can't be 0 or less than 0");
+            }
+            Radius = r;
+        }
+        // S = π*r^2
+        public double Square()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        // P = π*2r
+        public double Perimeter()
+        {
+            return Math.PI * 2 * Radius;
+        }
+    }
+}
---
+++ 3rd lab/3rd lab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3rd_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(3.5);
+            Console.WriteLine($"Square of c1 : {c1.Square()} and perimeter is {c1.Perimeter()}");
+            try
+            {
+                Circle c2 = new Circle(0);
+                Circle c3 = new Circle(-5);
+            }
+            catch (ArgumentException)
+            {
+
+                Console.WriteLine("You can't instantiate circles with radius less or equal to 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
