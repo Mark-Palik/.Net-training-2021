aa9b93cfd62073793a1c734a7ebe7f5e9def4d5c
Mark-Palik <Mark.Palik9322@gmail.com>
2021-03-13 19:41:24 +0300

    added input hint

--- 2nd lab/2nd lab/Program.cs
+++ 2nd lab/2nd lab/Program.cs
@@ -15,6 +15,7 @@ namespace _2nd_lab
             OneDimensionalArrayTask();
 
             int[,] array = new int[3, 3];
+            Console.WriteLine("Enter elements of 2d array one at a time");
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
